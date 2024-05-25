45740170095cd9008a81c9a31fc321777b0d5cea
andrei <loseu.andre.g@gmail.com>
2024-05-25 16:45:28 +0300

    publick S7Error

--- lib/dart_snap7.dart
+++ lib/dart_snap7.dart
@@ -1,3 +1,4 @@
 export 'package:dart_snap7/src/client.dart';
 export 'package:dart_snap7/src/async_client/async_client.dart';
 export 'package:dart_snap7/src/multi_request.dart';
+export 'package:dart_snap7/src/s7_types.dart';
